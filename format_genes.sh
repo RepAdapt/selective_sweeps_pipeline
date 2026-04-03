54750926ea24f5ef6b1afff0b9f010e560cf5e59
Gabriele <g.nocchi@hotmail.com>
2026-04-03 20:26:20 +0200

    Update format_genes.sh

--- format_genes.sh
+++ format_genes.sh
@@ -4,7 +4,7 @@ awk -F'\t' '$3 == "gene"' *.gff | awk '{OFS="\t"}NR>1{print $1,$4-1000,$5+1000,$
 
 # splitting the genes in 999 files, so that later we can parallelize the sweeps analysis by running each file containing a list of approx. 20-50 genes (total number of genes/999 files) as a separate job (999 HPC jobs in total)
 
-mkdir res
+mkdir genes_files
 split --numeric-suffixes=1 -n l/999 -d genes_coord.txt splitted
-mv splitted* res
+mv splitted* genes_files
 find . -name "splitted*" > genes_files_list.txt
