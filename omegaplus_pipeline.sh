feceb081a89cec9a66152085b65624a82159bdc2
Gabriele <g.nocchi@hotmail.com>
2026-04-08 11:46:04 +0200

    Update omegaplus_pipeline.sh

--- omegaplus_pipeline.sh
+++ omegaplus_pipeline.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=def-yeaman
 
 # This script runs OmegaPlus by gene using 2 CPUs threads. You can adjust the number of threads in the SLURM specification above AND in the OmegaPlus command at line 38. There is little to no advantage in using more threads.
-# We use a OmegaPlus grid of size of 3 for each gene which results in 3 measurements: one at the first SNP in a gene, one at the last SNP and one equidistant between these (approx. in the middle of the gene).
+# We use a OmegaPlus grid of size 3 for each gene which results in 3 measurements: one at the first SNP in a gene, one at the last SNP and one equidistant between these (approx. in the middle of the gene).
 # Adjust time limit according to the number of genes in your dataset (now 23 hours).
 
 
