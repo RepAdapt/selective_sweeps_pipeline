36a9a6d4b8fedb074e0e5262cf91a146c905fec0
Gabriele <g.nocchi@hotmail.com>
2026-04-08 14:42:39 +0200

    Add files via upload

--- omegaplus_pipeline.sh
+++ omegaplus_pipeline.sh
@@ -14,7 +14,7 @@ module load apptainer
 
 ### Formatting the gff: extracting genes, adding 1000 bp flanks on either side and saving original coordinates (without flanks) ###
 
-awk -F'\t' '$3 == "gene"' *.gff | awk '{OFS="\t"}NR>1{print $1,$4-1000,$5+1000,$1":"$4"-"$5}' | awk '{OFS="\t"}{if ($2 > 0)  print $1":"$2"-"$3,$4; else print $1":"1"-"$3,$4;}' > genes_coord.txt
+awk -F'\t' '$3 == "gene"' *.gff | awk '{OFS="\t"}{print $1,$4-1000,$5+1000,$1":"$4"-"$5}' | awk '{OFS="\t"}{if ($2 > 0)  print $1":"$2"-"$3,$4; else print $1":"1"-"$3,$4;}' > genes_coord.txt
 
 
 
