98b475842abb0413db914dad31b28b9b2dcfbcb4
Gabriele <g.nocchi@hotmail.com>
2026-04-08 11:25:05 +0200

    Add files via upload

---
+++ omegaplus_pipeline.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --time=0-23:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH --account=def-yeaman
+
+# This script runs OmegaPlus by gene using 2 threads. You can adjust the number of threads in the SLURM specification above AND in the OmegaPlus command below.
+# This uses a grid of size 3 for each gene whcih results in 3 measurements: one at the first SNP in a gene, one at the last SNP and one equidistant between these (approx. in the middle of the gene).
+# Increase time limit according to your dataset
+
+
+### Formatting the gff: extracting genes, adding 1000 bp flanks on either side and also saving original coordinates (without flanks) ###
+
+awk -F'\t' '$3 == "gene"' *.gff | awk '{OFS="\t"}NR>1{print $1,$4-1000,$5+1000,$1":"$4"-"$5}' | awk '{OFS="\t"}{if ($2 > 0)  print $1":"$2"-"$3,$4; else print $1":"1"-"$3,$4;}' > genes_coord.txt
+
+
+
+
+### OmegaPlus ###
+
+mkdir results
+
+# Specify your species file name. It also needs a VCF index file which can be generated with tabix: tabix -p final_variants.vcf.gz
+
+FILE="final_variants.vcf.gz"
+
+
+# The code below loop over all genes. For each gene, extract the gene region with 1000 bp flanking on either side from the VCF using bcftools
+# Then OmegaPlus is run on each gene using a grid size of 3, minwin 500 and maxwin 10000
+# Each output is given the original (start and end without added flanks) gene name as CHROM:start-end 
+
+
+regex='(.+)	(.+)'
+
+
+while read p; do
+
+if [[ $p =~ $regex ]];  then 	apptainer exec singularity/bcftools\:1.16--hfe4b78e_1 bcftools view $FILE --regions ${BASH_REMATCH[1]} -Ov -o $FILE\_${BASH_REMATCH[2]}\.vcf; apptainer run singularity/OmegaPlus.sif -input $FILE\_${BASH_REMATCH[2]}\.vcf -minwin 500 -maxwin 100000 -grid 3 -name $FILE\_${BASH_REMATCH[2]} -seed 12345 -threads 2; fi
+	rm $FILE\_${BASH_REMATCH[2]}\.vcf
+done < genes_coord.txt
+
+mv OmegaPlus_Report* results
+mv OmegaPlus_Info* results
