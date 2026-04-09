7a1dd019d3512877380430b00605f0e8360a83cd
Gabriele <g.nocchi@hotmail.com>
2026-04-09 10:49:12 +0200

    Add files via upload

--- omegaplus_pipeline.sh
+++ omegaplus_pipeline.sh
@@ -38,7 +38,7 @@ regex='(.+)	(.+)'
 while read p; do
 
 if [[ $p =~ $regex ]];  then 	apptainer exec apptainer/bcftools\:1.16--hfe4b78e_1 bcftools view $FILE --regions ${BASH_REMATCH[1]} -Ov -o $FILE\_${BASH_REMATCH[2]}\.vcf; apptainer run apptainer/OmegaPlus.sif -input $FILE\_${BASH_REMATCH[2]}\.vcf -minwin 500 -maxwin 100000 -grid 3 -name $FILE\_${BASH_REMATCH[2]} -seed 12345 -threads 2; fi
-	rm $FILE\_${BASH_REMATCH[2]}\.vcf
+        rm $FILE\_${BASH_REMATCH[2]}\.vcf
         tail -n +3 OmegaPlus_Report.*.vcf.gz_${BASH_REMATCH[2]} | awk -v var="${BASH_REMATCH[2]}" 'BEGIN {OFS="\t"} {print var, $2}' >> temp.txt	
         rm OmegaPlus_Report*
         rm OmegaPlus_Info*
