63356d9fe484e969ac19ee8afeb199ec6f195465
Gabriele <g.nocchi@hotmail.com>
2026-04-16 14:00:17 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=0-23:00
+#SBATCH --time=0-48:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=def-yeaman
 
 module load apptainer
 
-./omegaplus_pipeline.sh -vcf final_variants.vcf.gz -gff GCF_000001735.4_TAIR10.1_genomic_CHR_MATCHED.gff
+./omegaplus_pipeline.sh -vcf final_variants.vcf.gz -gff Egrandis1_0_genomic_CHR_MATCHED.gff
