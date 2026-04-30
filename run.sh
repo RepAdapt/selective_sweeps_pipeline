8e218abe9243109c2b71b60cffce5d74eaa1abe8
Gabriele <g.nocchi@hotmail.com>
2026-04-30 17:23:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=def-yeaman
 
+# change run time according to your dataset size
+# number of cpu per task needs to reflect the number of threads used by omegaplus, which I set to 2 in the omegaplus_pipeline.sh script
+
 module load apptainer
 
 ./omegaplus_pipeline.sh -vcf final_variants.vcf.gz -gff Egrandis1_0_genomic_CHR_MATCHED.gff
